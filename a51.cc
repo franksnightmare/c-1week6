6b85691ad5ce50f02d72680a4bebceb85143975b
franksnightmare <chnstone13backup@gmail.com>
2016-10-17 21:00:46 +0200

    Update a51.cc

--- a51.cc
+++ a51.cc
@@ -1,6 +1,17 @@
+//Once a file has been opened in read and write mode, 
+//the << operator can be used to insert information
+//into the file, while the >> operator may be used to
+//extract information from the file. These operations
+//may be performed in any order, but a seekg or seekp
+//operation is required when switching between insertions
+//and extractions. The seek operation is used to activate
+//the stream's data used for reading or those used for
+//writing (and vice versa). 
+
     cout << "extracted first number: " << no1 << '\n';
 
     istr.seekg(0);
+    
     istr.str(argv[2]);
     size_t no2 = 0;
     istr >> no2;
